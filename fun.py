a61534aac8a194add8195f3255eb80ea008dabc0
Admin <admin@iMac.local>
2019-08-26 23:52:43 -0400

    Appointmen_v2, sort

--- fun.py
+++ fun.py
@@ -1,7 +1,7 @@
 
 def read_data():
 	import csv
-	with open('/Users/admin/Desktop/test_data_room2.csv','rb') as csvfile:
+	with open('/Users/admin/Desktop/findroom/test_data_room2.csv','rb') as csvfile:
 #		data = list(csv.reader(csvfile))
 		return list(csv.reader(csvfile))
 	#	return data
@@ -36,3 +36,41 @@ def appointments(data_date):
 	for i in range(1,len(data_date)):
 		appointments.append((data_date[1][2],data_date[1][3]))
 	return appointments
+
+def Remove(duplicate): 
+    final_list = [] 
+    for num in duplicate: 
+        if num not in final_list: 
+            final_list.append(num) 
+    return final_list 
+      
+# # Driver Code 
+# duplicate = [2, 4, 10, 20, 5, 2, 20, 4] 
+# print(Remove(duplicate)) 
+
+rooms = []
+for i in range(1,len(data)):
+	rooms.append(data[i][0])
+#return rooms
+
+rooms.sort()
+
+
+def appointments_v2(data_date):
+	for i in range(1,len(data_date)):
+		data_date[i].append((data_date[1][2],data_date[1][3]))
+	return appointments
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
